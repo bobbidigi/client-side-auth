ad4915843dd69222ee295a07c5eef481e41cc203
bobbidigi <bobbidigi34@gmail.com>
2019-11-01 13:58:11 -0400

    Add logout

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import "./App.css"
 import React from "react"
 import {Link, Route} from 'react-router-dom'
+import {getToken} from '../utils/api'
 import ProtectedRoute from './ProtectedRoute'
 import Signin from './Signin'
 import Account from './Account'
-
+import Logout from './Logout'
 
 function App() {
+
+	const signedIn = getToken()
+
 	return (
 		<div className="wrapper">
 			<nav>
 				<Link to="/">Home</Link>
-				<Link to="/signin">Sign In</Link>
-				<Link to="/account">My Account</Link>
+				{!signedIn && <Link to="/signin">Sign In</Link>}
+				{signedIn && <Link to="/account">My Account</Link>}
+				{signedIn && <Link to="/logout">Log Out</Link>}
 			</nav>
 			<Route exact path='/signin' component={Signin}/>
 			<ProtectedRoute exact path='/account' component={Account}/>
+			<ProtectedRoute exact path='/logout' component={Logout}/>
 		</div>
 	)
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
+export function getToken(){
+    return localStorage.getItem("token")
+}
+
 export default function (){
     return axios.create({
         baseURL: "http://localhost:8080",
         headers: {
-            Authorization: localStorage.getItem("token"),
+            Authorization: getToken(),
         },
     })
 }
